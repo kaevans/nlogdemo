aa65f115c50aa5ac4e1cddda177cacb292efc6f4
Kirk Evans <kirke@microsoft.com>
2020-11-19 06:00:12 -0600

    Updated URL call example

--- NLogDemo/Program.cs
+++ NLogDemo/Program.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,54 +16,51 @@ namespace NLogDemo
         private static readonly HttpClient client = new HttpClient();
 
         static void Main(string[] args)
-        {
-            bool flag = true;
-
+        {            
+            client.Timeout = new TimeSpan(0, 0, 3);
             do
             {
                 var log = LogManager.GetCurrentClassLogger();
                 log.Debug("This is a debug message");
-                log.Error(new Exception(), "This is an error message");
-                log.Fatal("This is a fatal message");
+                                
+                log.Info("This is a message from {User}", WindowsIdentity.GetCurrent().Name);
 
-                log.Info("This is a message from {User}", "Mickey Donovan");
+                GetHttpStringAsync("http://www.msn.com").GetAwaiter().GetResult();
+                GetHttpStringAsync("http://www.microsoft.com").GetAwaiter().GetResult();
+                GetHttpStringAsync("http://www.bing.com").GetAwaiter().GetResult();
+                GetHttpStringAsync("https://doesntexist/").GetAwaiter().GetResult();
 
-
-                var responseString = string.Empty;
-                try
-                {
-                    responseString = client.GetStringAsync("http://www.msn.com/").GetAwaiter().GetResult();
-                    log.Trace(responseString);
-                    log.Info("successfully called msn");
-                }
-                catch(System.Exception oops)
-                {
-                    log.Error(oops, "Calling msn");
-                }
-                try
-                {
-                    responseString = client.GetStringAsync("http://www.microsoft.com/").GetAwaiter().GetResult();
-                    log.Trace(responseString);
-                    log.Info("successfully called microsoft");
-                }
-                catch (System.Exception oops)
-                {
-                    log.Error(oops, "Calling microsoft");
-                }
-                try
-                {
-                    responseString = client.GetStringAsync("http://www.bing.com/").GetAwaiter().GetResult();
-                    log.Trace(responseString);
-                    log.Info("successfully called bing");
-                }
-                catch (System.Exception oops)
-                {
-                    log.Error(oops, "calling bing");
-                }
                 System.Threading.Thread.Sleep(1000);
 
             } 
-            while (flag == true);
+            while (true);
+        }
+
+        private static async Task<string> GetHttpStringAsync(string url)
+        {
+            //Set timeout for 3 seconds
+            
+            var log = LogManager.GetCurrentClassLogger();
+            
+            var responseString = default(string);
+
+            try
+            {
+                var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                log.Info("{Status}: {Url}", response.StatusCode, url);
+                
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                log.Trace(content);
+
+            }
+            catch (System.Exception oops)
+            {
+                log.Error(oops, "Error calling {Url}", url);
+            }
+
+            return responseString;
         }
     }
 }
